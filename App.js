e5865bb2648f1c9d444a9fa9a5b5ad925e44ca85
Kimsuseong <kimsusung55@gmail.com>
2020-11-21 14:06:16 +0900

    Adding a Card View

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, TextInput, View, Text} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.appTitle}>Hello Todolist</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder="Add an item!" />
+      </View>
     </SafeAreaView>
   );
 };
@@ -19,9 +22,28 @@ const styles = StyleSheet.create({
     fontSize: 36,
     marginTop: 30,
     marginBottom: 30,
-    fontWeight: 'normal',
+    fontWeight: '300',
     textAlign: 'center',
     backgroundColor: '#3143e8',
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 10, // to provide rounded corners
+    borderTopRightRadius: 10, // to provide rounded corners
+    borderBottomRightRadius: 10, // to to provide rounded coners
+    borderBottomLeftRadius: 10, // to to provide rounded coners
+    marginLeft: 15,
+    marginRight: 15,
+    marginBottom: 15,
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft: 20,
+    marginRight: 20,
   },
 });
 
